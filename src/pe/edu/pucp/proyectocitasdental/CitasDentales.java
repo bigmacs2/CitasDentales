a96d993c1ff04912378a9aed467468aea403dac6
Emanuel Garcia Montenegro <egm1402@hotmail.com>
2025-04-18 17:58:26 -0500

    Se agrega el mensaje en el main

--- src/pe/edu/pucp/proyectocitasdental/CitasDentales.java
+++ src/pe/edu/pucp/proyectocitasdental/CitasDentales.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pe.edu.pucp.proyectocitasdental;
+import java.util.*;
 
 public class CitasDentales {
 
@@ -11,6 +12,7 @@ public class CitasDentales {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Mensaje de prueba");
     }
     
 }
