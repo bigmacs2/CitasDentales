bc71f71f7773256cf203d98cbff674a6f73dfd0d
Emanuel Garcia Montenegro <egm1402@hotmail.com>
2025-04-18 17:46:16 -0500

    Se agrega el proyecto

---
+++ src/pe/edu/pucp/proyectocitasdental/reportes/Ireporte.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.pucp.proyectocitasdental.reportes;
+
+public interface Ireporte {
+    
+}
